e4ba4157dead26ec1cc856936916b3fe9f9e767e
stackprobe <stackprobes@gmail.com>
2019-08-22 20:57:47 +0900

    Continental

--- ornithopter/tools_barnatsutobi/instagram/update.bat
+++ ornithopter/tools_barnatsutobi/instagram/update.bat
@@ -8,7 +8,7 @@ C:\app\Kit\HGet\HGet.exe ^
 	/U "https://www.instagram.com/barnatsutobi/" ^
 	/RSF C:\appdata\barnatsutobi\instagram\successful.flg ^
 	/RHF C:\appdata\barnatsutobi\instagram\res-header.out ^
-	/RBF C:\appdata\barnatsutobi\instagram\res-body.out ^
+	/RBF C:\appdata\barnatsutobi\instagram\res-body.out
 
 IF NOT EXIST C:\appdata\barnatsutobi\instagram\successful.flg GOTO END
 mkIndexHtml.exe C:\appdata\barnatsutobi\instagram\res-body.out ..\..\home_barnatsutobi\instagram\index.html
