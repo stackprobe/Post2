fffcb9919c2a9220d1a1930d045d79ebbb5e6b21
stackprobe <stackprobes@gmail.com>
2019-10-09 13:05:55 +0900

    Sword

--- ornithopter/cs/Corona/Corona/Utils/DenebolaToolkit.cs
+++ ornithopter/cs/Corona/Corona/Utils/DenebolaToolkit.cs
@@ -46,6 +46,40 @@ namespace Charlotte.Utils
 				return outLines[0];
 			}
 		}
+
+		public static string GetFairFullPath(string path)
+		{
+			using (WorkingDir wd = new WorkingDir())
+			{
+				string argsFile = wd.MakePath();
+				string outFile = wd.MakePath();
+				string molp = SecurityTools.MakePassword_9();
+
+				string[] args =
+				{
+					"//O",
+					outFile,
+					"//MOLP",
+					molp,
+					"//-E",
+					"//$",
+					JString.AsLine(path),
+				};
+
+				File.WriteAllLines(argsFile, args, StringTools.ENCODING_SJIS);
+
+				ProcessTools.Start(@"C:\Factory\Bodewig\DenebolaToolkit\FairFullPath.exe", "//R \"" + argsFile + "\"").WaitForExit();
+
+				string[] outLines = File.ReadAllLines(outFile, StringTools.ENCODING_SJIS)
+					.Where(v => v.StartsWith(molp))
+					.Select(v => v.Substring(molp.Length)).ToArray();
+
+				if (outLines.Length != 1)
+					throw new Exception("不正なパス文字列です。");
+
+				return outLines[0];
+			}
+		}
 	}
 
 	// < sync
