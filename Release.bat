630ac42f3b8291e8c8fabd4791bc4893e37fb08b
stackprobe <stackprobes@gmail.com>
2020-07-06 14:43:19 +0900

    Berry

--- Release.bat
+++ Release.bat
@@ -3,8 +3,4 @@ IF NOT EXIST Post2\Post2.exe START ?_No_Post2_exe
 C:\Factory\Tools\Cluster.exe /OW /M out\ornithopter.clu ornithopter
 C:\Factory\Tools\Cluster.exe /OW /M out\Post2.clu Post2
 
-rem Å°Å°Å°Å°Å°Å°Å°Å°Å°Å°Å°Å°Å°Å°
-rem Å°Å°Å° syncRev ÇµÇƒÇÀ Å°Å°Å°
-rem Å°Å°Å°Å°Å°Å°Å°Å°Å°Å°Å°Å°Å°Å°
-
 IF NOT "%1" == "/-P" PAUSE
