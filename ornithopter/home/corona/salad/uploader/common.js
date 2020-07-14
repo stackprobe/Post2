d372f72be761cd54ba9939be36a390d960093701
stackprobe <stackprobes@gmail.com>
2020-07-15 05:54:12 +0900

    Scarlet

--- ornithopter/home/corona/salad/uploader/common.js
+++ ornithopter/home/corona/salad/uploader/common.js
@@ -16,43 +16,57 @@ function Forwarded(reaction) {
 	});
 }
 
-var PageLoadingScreenTags;
+var PageLoadingTags = null;
 
 function PageLoading() {
-	var b = document.createElement("div");
+	if(PageLoadingTags == null) {
+		var b = document.createElement("div");
 
-	b.style.position = "fixed";
-	b.style.top = "0px";
-	b.style.left = "0px";
-	b.style.width = "100%";
-	b.style.height = "100%";
-	b.style.backgroundColor = "#000000";
-	b.style.opacity = "0.5";
-	b.style.zIndex = "1";
+		b.style.position = "fixed";
+		b.style.top = "0px";
+		b.style.left = "0px";
+		b.style.width = "100%";
+		b.style.height = "100%";
+		b.style.backgroundColor = "#000000";
+		b.style.opacity = "0.5";
+		b.style.zIndex = "1";
 
-	var p = document.createElement("div");
+		var p = document.createElement("div");
 
-	p.style.position = "fixed";
-	p.style.top = "calc(50% - 64px)";
-	p.style.left = "0px";
-	p.style.width = "100%";
-	p.style.height = "128px";
-	p.style.backgroundColor = "#ffffff";
-	p.style.opacity = "1.0";
-	p.style.zIndex = "2";
+		p.style.position = "fixed";
+		p.style.top = "calc(50% - 64px)";
+		p.style.left = "0px";
+		p.style.width = "100%";
+		p.style.height = "128px";
+		p.style.backgroundColor = "#ffffff";
+		p.style.opacity = "1.0";
+		p.style.zIndex = "2";
 
-	b.style.padding = "0px"; // HACK: common.css
-	p.style.padding = "0px"; // HACK: common.css
+		b.style.padding = "0px"; // HACK: common.css
+		p.style.padding = "0px"; // HACK: common.css
 
-	p.innerHTML = "<div style='padding: 0px; text-align: center'><img src='flat-preloaders/Preloader_2.gif'/></div>";
+		p.innerHTML = "<div style='padding: 0px; text-align: center'><img src='flat-preloaders/Preloader_2.gif'/></div>";
 
-	Rose_Append(document.body, b);
-	Rose_Append(document.body, p);
+		Rose_Append(document.body, b);
+		Rose_Append(document.body, p);
 
-	PageLoadingScreenTags = [ b, p ];
+		PageLoadingTags = [ b, p ];
+	}
+
+	PageLoadingTags[0].style.visibility = "";
+	PageLoadingTags[1].style.visibility = "";
 }
 
 function PageLoaded() {
-	Rose_Remove(PageLoadingScreenTags[0]);
-	Rose_Remove(PageLoadingScreenTags[1]);
+	PageLoadingTags[0].style.visibility = "hidden";
+	PageLoadingTags[1].style.visibility = "hidden";
+
+// old
+/*
+	Rose_Remove(PageLoadingTags[0]);
+	Rose_Remove(PageLoadingTags[1]);
+
+	PageLoadingTags = null;
+*/
+
 }
