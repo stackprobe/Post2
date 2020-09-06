79d0107c1b0657743596323a8db2deaf7aba0630
stackprobe <stackprobes@gmail.com>
2020-09-06 18:56:56 +0900

    Charon

--- Conv/ToOneHtml/Make_20200904.bat
+++
@@ -1,2 +0,0 @@
-ToOneHtml C:\Dev\Main\Post2\ornithopter\home\coffee\hatena\a20200904_PosixTime\main.txt C:\temp\main_out.txt
-PAUSE
---
+++ wb/t20200904/MakeHatena20200905.bat
@@ -0,0 +1,2 @@
+C:\Factory\SubTools\ToOneHtml.exe ..\..\ornithopter\home\coffee\hatena\a20200904_PosixTime\main.txt C:\temp\main_out.txt
+PAUSE
