4a123813c074d8486afbf4e99b5bb12e3dc98967
stackprobe <stackprobes@gmail.com>
2020-09-06 19:20:04 +0900

    Neptune

---
+++ Conv/ToOneHtml/MakeHatena20200905.bat
@@ -0,0 +1,2 @@
+ToOneHtml.exe ..\..\ornithopter\home\coffee\hatena\a20200904_PosixTime\main.txt C:\temp\main_out.txt
+START C:\temp
--- wb/t20200904/MakeHatena20200905.bat
+++
@@ -1,2 +0,0 @@
-C:\Factory\SubTools\ToOneHtml.exe ..\..\ornithopter\home\coffee\hatena\a20200904_PosixTime\main.txt C:\temp\main_out.txt
-PAUSE
